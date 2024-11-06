de4f51d9f201dde2a33ee5f63b01b5ebec904e52
Ifolo-243 <ifolochukwu.ugbechiepau.edu.ng>
2024-11-06 21:05:52 +0100

    (practice_5)

---
+++ week 4/pracice_5/target/debug/deps/pracice_5.d
@@ -0,0 +1,5 @@
+C:\Users\USER\OneDrive\Documentos\i.ugbechieCSC101\week 4\pracice_5\target\debug\deps\pracice_5.exe: src/main.rs
+
+C:\Users\USER\OneDrive\Documentos\i.ugbechieCSC101\week 4\pracice_5\target\debug\deps\pracice_5.d: src/main.rs
+
+src/main.rs:
---
+++ week 4/pracice_5/target/debug/pracice_5.d
@@ -0,0 +1,1 @@
+C:\Users\USER\OneDrive\Documentos\i.ugbechieCSC101\week\ 4\pracice_5\target\debug\pracice_5.exe: C:\Users\USER\OneDrive\Documentos\i.ugbechieCSC101\week\ 4\pracice_5\src\main.rs
